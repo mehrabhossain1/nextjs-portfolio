f63b2248b7bc871f5c2e740a7edac5fe5043ac64
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-05 20:18:25 +0600

    creating Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,17 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>My Portfolio</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl">Hey, I&apos;m Mehrab Hossain</h1>
+      <p>
+        A Frontend focused Mern Stack Developer building the Frontend of
+        Websites and Web Applications that leads to the success of the overall
+        product.
+      </p>
+      <div className="flex">
+        <button className="btn btn-info">Download Resume</button>
+        <button className="btn btn-info">Resume</button>
+      </div>
     </div>
   );
 };
